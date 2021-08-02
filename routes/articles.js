b282a9690a4a42d0ec45dacf92601efe947180e5
chawlajay <chawlajay111@gmail.com>
2021-08-02 15:13:19 +0530

    add bootstrap, new article file,show article cards

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-res.send('In articles');
+router.get('/new',(req,res)=>{              // route request by user/client
+res.render('articles/new');              // for view engine file
+});
+
+router.post('/',(req,res)=>{
+
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,21 @@ const articleRouter = require('./routes/articles')
 const app = express();
 
 app.set('view engine', 'ejs');
-
-app.use('/articles',articleRouter);
+app.use(express.static(__dirname + '/public'));
+app.use('/articles',articleRouter);            // route for user/client
 
 app.get('/',(req,res)=>{
-    res.render('index', {text : "hellow"});
+    const articles = [{
+        title : 'some article',
+        createdAt : new Date(),
+        description : 'some description'
+    },
+    {
+        title : 'some article',
+        createdAt : new Date(),
+        description : 'some description'
+    }];
+    res.render('articles/index', {articles : articles});  // file path for rendering by view engine of EJS
 });
 
 app.listen(5000, ()=>{
