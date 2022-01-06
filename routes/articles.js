022172db4d5c140a26f5ee49f7d842353dc73f3b
chawlajay <chawlajay111@gmail.com>
2022-01-06 14:11:28 +0530

    add all files

--- routes/articles.js
+++ routes/articles.js
@@ -6,30 +6,52 @@ router.get('/new',(req,res)=>{              // route request by user/client  - h
 res.render('articles/new',{article : new Article()});              // for view engine file
 });
 
-router.get('/:id', async (req,res)=>{
+router.get('/edit/:id',async (req,res)=>{              // route request by user/client  - here it is /articles/new
     const article = await Article.findById(req.params.id);
+    
+    res.render('articles/edit',{article : article});              // for view engine file
+    });
+    
+
+router.get('/:slug', async (req,res)=>{
+    const article = await Article.findOne({slug: req.params.slug});
     if(article == null) res.redirect('/');
     res.render('articles/show', {article : article});
 });
 
-router.post('/', async (req,res)=>{
-let article = new Article({
-title: req.body.title,
-description: req.body.description,
-markdown: req.body.markdown
+router.post('/', async (req,res,next)=>{
+req.article = new Article();
+next();
+}, saveArticleAndRedirect("new"));
+
+router.put('/:id', async (req,res,next)=>{
+    req.article = await Article.findById(req.params.id);
+    next();
+    }, saveArticleAndRedirect("edit"));    
+    
+router.delete('/:id', async (req,res)=>{
+await Article.findByIdAndDelete(req.params.id);
+res.redirect('/');
 });
 
-try{
-    article = await article.save(); // article ID
-    // console.log(article);
-    res.redirect(`/articles/${article.id}`);
+function saveArticleAndRedirect(path){
+return async (req,res)=>{
+    let article = req.article;
+        article.title = req.body.title;
+        article.description = req.body.description;
+        article.markdown = req.body.markdown;
+        
+        try{
+            article = await article.save(); // article ID
+            // console.log(article);
+            res.redirect(`/articles/${article.slug}`);
+        }
+        catch (e){
+            // console.log(article.id);
+            console.log(e);
+            res.render(`articles/${path}`, {article : article})
+        }
 }
-catch (e){
-    // console.log(article.id);
-    console.log(e);
-    res.render('articles/new', {article : article})
 }
-
-});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const mongoose = require('mongoose');
 const articleRouter = require('./routes/articles');
 const Article = require('./models/article');
+const methodOverride = require('method-override');
 const app = express();
 
-mongoose.connect('mongodb://localhost/blog' , {useNewUrlParser: true, useUnifiedTopology: true},()=>{
+mongoose.connect('mongodb://localhost/blog' , {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true},()=>{
     new Promise((resolve, reject) => {
         reject('error');
       }).catch((error) => {});
@@ -12,7 +13,7 @@ mongoose.connect('mongodb://localhost/blog' , {useNewUrlParser: true, useUnified
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended : false}))   // we can access different parameters from our article form inside our articles route using req.body
-
+app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));  // to use static css files
 
 app.get('/', async (req,res)=>{
